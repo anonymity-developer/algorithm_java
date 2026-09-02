addbeaac40031597489fa07bafd0c4c1010a1355
백지원 <anonymity.developer@gmail.com>
2026-09-02 09:32:54 +0900

    init

---
+++ swea/src/swea_base.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class swea_base{
+}
+
+class Soltion {
+
+    public static void main(String[] args) throws Exception {
+
+        System.setIn(new FileInputStream("res/문제명/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+        	
+            // 풀이
+        	int answer = 0;
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
